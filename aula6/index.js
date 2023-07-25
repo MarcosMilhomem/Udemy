2943f581af00dd08cc8b05b64d7742e852db496e
Marcos Milhomem <marcosapolo511@gmail.com>
2023-07-25 00:32:48 -0300

    aula6

---
+++ aula6/index.js
@@ -0,0 +1,11 @@
+//Nâo podemos criar vriávies com palavras reservadas
+//Varáveis prcisam ter nomes significativos
+//Não podde começar o nome de uma variável com um número
+//Utilizamos camelCase
+//Case-sensitive
+//Não podemos redeclarar variáveis com let
+//NÃO UTILIZAR VAR, UTILIZAR LET
+let nomeCliente = 'Luiz';
+nomeCliente = 'Otávio';
+
+console.log(nomeCliente);
