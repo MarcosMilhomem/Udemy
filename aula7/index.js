e03e686b170725cb69923a6cb7dfef225af7bc61
Marcos Milhomem <marcosapolo511@gmail.com>
2023-07-26 19:17:21 -0300

    aula8

---
+++ aula7/index.js
@@ -0,0 +1,7 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5
+console.log(resultadoTriplicado);
---
+++ aula8/index.js
@@ -0,0 +1,9 @@
+const nome = 'Marcos Apolo';
+const sobrenome = 'Milhomem';
+const idade = 18;
+const peso = 76;
+const altura = 1.80;
+let anoNascimento;
+
+let imc = peso / (altura * altura)
+alert(`${nome}, ${sobrenome}, tem, ${idade}, anos, e pesa ${peso}. Seu IMC é ${imc}`)
