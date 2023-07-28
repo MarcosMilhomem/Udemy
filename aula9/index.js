b9681d4da81c415518a8fbe87560074b344d00fa
Marcos Milhomem <marcosapolo511@gmail.com>
2023-07-28 13:01:38 -0300

    aula9

---
+++ aula9/index.js
@@ -0,0 +1,8 @@
+//com o VAR é possível mudar o valor da variável no decorrer do código.
+ var nome = 'Apolo';
+ var nome = 'Marcos';
+ console.log(nome);
+
+//Com o LET não se torna possível mudar o valor da variável nem reutilizar uma variável já existente.
+ let name = 'Apolo';
+ let name = 'Apolo';
