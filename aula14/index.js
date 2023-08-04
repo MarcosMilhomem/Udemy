257ac522abbb33f81b75f77faa9d564b961becc9
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-04 15:39:37 -0300

    aula14

--- aula14/index.js
+++ aula14/index.js
@@ -2,14 +2,9 @@
 let num1 = 0.7; //number
 let num2 = 0.1; //number
 
-num1 += num2; //0.8
-num1 += num2; //0.9
-num1 += num2; //1.0
-num1 += num2; //1.1
-num1 += num2; //1.2
-num1 += num2; //1.3
-num1 += num2; //1.4
-num1 += num2; //1.5
+// num1 = ((num1 * 100) + (num2 * 100) / 100);
+// num1 = ((num1 * 100) + (num2 * 100) / 100);
+// num1 = ((num1 * 100) + (num2 * 100) / 100);
 
 num1 = parseFloat(num1.toFixed(2));
 
