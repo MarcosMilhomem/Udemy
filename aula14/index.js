5ef363c9101d7d6ed50784217fc3d3c7b3cd946c
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-04 14:38:19 -0300

    aula14

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,20 @@
-let num1 = 10; //number
-let num2 = 2.5; //number
+//IEEE 754-2008
+let num1 = 0.7; //number
+let num2 = 0.1; //number
+
+num1 += num2; //0.8
+num1 += num2; //0.9
+num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2; //1.3
+num1 += num2; //1.4
+num1 += num2; //1.5
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
 
 // console.log (num1.toString() + num2);
 //num1 = num1.toString(); - trnaformar permanentemente em string
@@ -8,5 +23,4 @@ let num2 = 2.5; //number
 // console.log(Number.isInteger(num1));
 // console.log(Number.isNaN(temp));
 // let temp = num1 * 'Olá';
-
-console.log(temp)
+// console.log(temp)
