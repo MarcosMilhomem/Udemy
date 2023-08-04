9f4ece186411f236b219c7351727f4c57f6a0a03
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-04 16:10:48 -0300

    aula15

---
+++ aula15/index.js
@@ -0,0 +1,7 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // arredonda o valor pra cima
+// let num2 = Math.ceil(num1); // arredonda o valor pra baixo
+// let num2 = Math.round(num1); // distribui de acordo, abaixo da metade ele arrredonda pro mais baixo, da metade pra cima arredonda pro mais alto.
+let num2 = num1;
+
+ console.log(num2);
