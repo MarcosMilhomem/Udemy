27c353ed4b4184fb66767e866a610e41dc208a75
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-04 02:04:01 -0300

    aula14

---
+++ aula14/index.js
@@ -0,0 +1,12 @@
+let num1 = 10; //number
+let num2 = 2.5; //number
+
+// console.log (num1.toString() + num2);
+//num1 = num1.toString(); - trnaformar permanentemente em string
+// console.log(num1.toString(2)) - representação binária do valor da variável
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// console.log(Number.isNaN(temp));
+// let temp = num1 * 'Olá';
+
+console.log(temp)
