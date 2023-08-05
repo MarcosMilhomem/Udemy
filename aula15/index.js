640e73e9aeceb2f4c44ea7879b367074abebc55f
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-04 22:47:44 -0300

    aula15

--- aula15/index.js
+++ aula15/index.js
@@ -1,7 +1,8 @@
-let num1 = 9.54578;
+console.log(10-0/0)
+
 // let num2 = Math.floor(num1); // arredonda o valor pra cima
 // let num2 = Math.ceil(num1); // arredonda o valor pra baixo
 // let num2 = Math.round(num1); // distribui de acordo, abaixo da metade ele arrredonda pro mais baixo, da metade pra cima arredonda pro mais alto.
-let num2 = num1;
-
- console.log(num2);
+// console.log(Math.max(1,2,3,4,5,-50,-123,23,5463,435,8,244,134));
+// console.log(Math.min(1,2,3,4,5,-50,-123,23,5463,435,8,244,134));
+// const aleatorio = Math.round(Math.random() * (10 * 5) + 5); // Random para criar números aleatórios entre 0 e 1.
