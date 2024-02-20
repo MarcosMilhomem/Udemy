589106829bcc3c75c34f90d37a0a97ba963c2f61
Marcos Milhomem <marcosapolo511@gmail.com>
2024-02-20 00:40:22 -0300

    estudo

---
+++ aula21/index.js
@@ -0,0 +1,14 @@
+/* 
+Operadores de comparação
+
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valore tipo)
+*/
+console.log(10 > 5);
+console.log(10 >= 11);
---
+++ aula22/index.js
@@ -0,0 +1,7 @@
+/* 
+Operadores Lógicos
+&& => AND ou E -- Todas as expressões precisam ser verdadeiras pra retornar true ou 
+|| => OR - OU
+! => NOT - NÃO
+*/
+console.log (true && true)
