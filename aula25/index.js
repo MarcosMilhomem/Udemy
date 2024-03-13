5ae13b2b132c10355f98344cc4577cad051e4374
Marcos Milhomem <marcosapolo511@gmail.com>
2024-03-13 01:09:46 -0300

    study

--- aula25/index.js
+++ aula25/index.js
@@ -1,7 +1,17 @@
 const numero = 10;
 
-if (numero >= 0 && numero <= 5) {
-    console.log('sim')
-} else {
-    
+if (numero <= 10) { //Verdadeira
+    console.log('O número é menor ou igual a 10')
 }
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre o e 5.')
+} else  if (numero >= 6 && numero <= 8) {
+    console.log('O núimero está entre 6 e 8')
+} else if (numero >= 9 && numero <= 11 ) {
+    console.log('O número está entre 9 e 11')
+} else {
+    console.log(' O número não está entre 0 e 11')
+}
+
+console.log('...Aqui vai o resto do código')
