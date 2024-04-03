62f3142872c2ecce1715be1f0f4ec1c41b4c9d09
Marcos Milhomem <marcosapolo511@gmail.com>
2024-04-03 01:05:58 -0300

    sttudy

--- aula26-demo/assets/js/index.js
+++ aula26-demo/assets/js/index.js
@@ -2,25 +2,42 @@ const form = document.querySelector ('#questions')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    console.log('Evento previnido')
-    setResult();
-    calculo();
+    const inputPeso = e.target.querySelector('#peso')
+    const inputAltura = e.target.querySelector('#altura')
+    
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if (!peso) {
+        setResult('Peso inválido', false)
+        return
+    }
+
+    if (!altura) {
+        setResult('Altura inválido')
+        return
+    }
+
+    const imc = getImc(peso, altura)
+    console.log(imc)
 });
 
-function setResult(msg) {
-    const result = document.querySelector ('#result');
-    result.innerHTML = '';
-    const p = document.createElement('p');
-    p.innerHTML = calculo();
-    result.appendChild(p);
+function getImc (peso, altura) {
+    const imc = peso / (altura ** 2)
+    return imc.toFixed(2)
 }
 
-function calculo() {
-    const weight = document.querySelector ('#peso')
-    const height = document.querySelector ('#altura')
+function criaP (className) {
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado')
+    return p;
+};
 
-    const weightMod = Number(weight)
-    const heightMod = Number(height)
-    
-    const equacao = weightMod / (heightMod * heightMod);
+function setResult(msg, isValid) {
+    const result = document.querySelector ('#result');
+    result.innerHTML = '';
+    result.appendChild(p);
+
+    const p = criaP();
+
 }
